3fef9a71b9dac42435279cb3580d48936039e6d3
MichaelLinch90 <michaellinch90@gmail.com>
2022-09-19 15:11:56 -0700

    completed crud functionality on bets

--- slick_fantasy/main_app/models.py
+++ slick_fantasy/main_app/models.py
@@ -1,3 +1,15 @@
+from pickle import FALSE
 from django.db import models
 
 # Create your models here.
+
+class Bet(models.Model):
+    name = models.CharField(max_length=50)
+    total_score = models.IntegerField(default=0)
+    wager = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    
+
--- slick_fantasy/main_app/views.py
+++ slick_fantasy/main_app/views.py
@@ -1,4 +1,7 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Bet
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse
 
 # Create your views here.
@@ -8,4 +11,31 @@ def home(request):
 
 # http://localhost:8000
 def bets(request):
-    return render(request, "bets.html")
+    bets = Bet.objects.all()
+    return render(request, "bets.html", {'bets' : bets})
+
+def bets_detail(request, bet_id):
+  bets = Bet.objects.get(id=bet_id)
+  return render(request, 'bets/detail.html', { 'bets': bets })
+
+class BetsCreate(CreateView):
+  model = Bet
+  fields = ['name', 'wager']
+
+  def get_success_url(self, **kwargs):
+        # http://127.0.0.1:8000/bets/9
+        # path('bets/<int:bet_id>/', views.bets_detail, name='detail'),
+        return reverse('detail', args=(self.object.id,))
+
+class BetsUpdate(UpdateView):
+  model = Bet
+  # Only allows user to update wager and name
+  fields = ['name', 'wager']
+
+  def get_success_url(self, **kwargs):
+        return reverse('detail', args=(self.object.id,))
+
+class BetsDelete(DeleteView):
+  model = Bet
+  success_url = '/bets/'
+    
