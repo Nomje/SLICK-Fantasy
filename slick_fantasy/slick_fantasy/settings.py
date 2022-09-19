3e5c61115e8ac6c55969c9d9feb967f9c1f38df1
vsalter <vsalter1@student.gsu.edu>
2022-09-19 18:31:57 -0400

    2nd commit

--- slick_fantasy/slick_fantasy/settings.py
+++ slick_fantasy/slick_fantasy/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'slick_fantasy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'SLICK',
     }
 }
 
