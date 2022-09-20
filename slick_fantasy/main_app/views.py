4b240323b74d279808fd6a0a262d9f6a689443bf
sparklingwaterlemon <michaelkim009@gmail.com>
2022-09-19 17:51:07 -0700

    1. Organized code (added path comments & fixed tab spacing) 2. Formatted Bet Detail Page (HTML/ CSS only no coding changes) 3. Added some available paths to NAV bar

--- slick_fantasy/main_app/views.py
+++ slick_fantasy/main_app/views.py
@@ -1,77 +1,98 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Bet, Team
+
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 
-# Create your views here.
-# http://localhost:8000
+
+# Home Page
+# http://localhost:8000/
 def home(request):
     return render(request, "home.html")
 
-# http://localhost:8000
+# Bets Index
+# http://localhost:8000/bets/
 def bets(request):
     bets = Bet.objects.all()
-    return render(request, "bets.html", {'bets' : bets})
+    return render(request, "bets.html", {
+        'bets': bets
+        })
 
+# Bets Detail
+# http://localhost:8000/bets/123/
 def bets_detail(request, bet_id):
-  bets = Bet.objects.get(id=bet_id)
-  teams_bets_doesnt_have = Team.objects.exclude(id__in=bets.teams.all().values_list('id'))
-  return render(request, 'bets/detail.html', { 'bets': bets, 
-  'teams' : teams_bets_doesnt_have})
+    bets = Bet.objects.get(id=bet_id)
+    teams_bets_doesnt_have = Team.objects.exclude(id__in=bets.teams.all().values_list('id'))
+    return render(request, 'bets/detail.html', {
+        'bets': bets,
+        'teams': teams_bets_doesnt_have
+    })
 
+# Create a New Bet
+# http://localhost:8000/bets/create/
 class BetsCreate(CreateView):
-  model = Bet
-  fields = ['name', 'wager']
+    model = Bet
+    fields = ['name', 'wager']
 
-  def get_success_url(self, **kwargs):
-        # http://127.0.0.1:8000/bets/9
+    def get_success_url(self, **kwargs):
+        # http://localhost:8000/bets/123/
+        # path('bets/<int:bet_id>/', views.bets_detail, name='detail'),
+        return reverse('detail', args=(self.object.id, ))
+
+# Update Bet
+# http://localhost:8000/bets/123/update/
+class BetsUpdate(UpdateView):
+    model = Bet
+    # Only allows user to update wager and name
+    fields = ['name', 'wager']
+
+    def get_success_url(self, **kwargs):
+        # back to details page of bet
         # path('bets/<int:bet_id>/', views.bets_detail, name='detail'),
         return reverse('detail', args=(self.object.id,))
 
-class BetsUpdate(UpdateView):
-  model = Bet
-  # Only allows user to update wager and name
-  fields = ['name', 'wager']
-
-  def get_success_url(self, **kwargs):
-        return reverse('detail', args=(self.object.id,))
-
+# Delete Bet
+# http://localhost:8000/bets/123/delete/
 class BetsDelete(DeleteView):
-  model = Bet
-  success_url = '/bets/'
+    model = Bet
+    success_url = '/bets/'
 
-# http://localhost:8000/toys/
+
+# List of Teams
+# http://localhost:8000/teams/
 class TeamList(ListView):
     model = Team
 
-# http://localhost:8000/team/1/
+# Team Detail
+# http://localhost:8000/teams/123/
 class TeamDetail(DetailView):
     model = Team
 
-
-# http://localhost:8000/team/create/
+# Team Create
+# http://localhost:8000/teams/create/
 class TeamCreate(CreateView):
     model = Team
     fields = '__all__'
 
-
-# http://localhost:8000/team/1/update/
+# Team Update
+# http://localhost:8000/teams/123/update/
 class TeamUpdate(UpdateView):
     model = Team
     fields = ['name', 'color']
 
-
-# http://localhost:8000/team/1/delete/
+# Team Delete
+# http://localhost:8000/teams/112/delete/
 class TeamDelete(DeleteView):
     model = Team
     success_url = '/team/'
 
-# http://localhost:8000/cats/2/assoc_toy/1/
+# Adding Teams to Bet
+# http://localhost:8000/bets/123/assoc_team/123/
 def assoc_team(request, bet_id, team_id):
     # id = pk
-    # SELECT name FROM cats WHERE id=team
     Bet.objects.get(id=bet_id).teams.add(team_id)
     return redirect('detail', bet_id=bet_id)
     
